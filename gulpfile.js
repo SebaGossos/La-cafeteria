f365952539737f3f99343b33b1bf6b2c3730ab6d
Sebastian <winigossos@gmail.com>
2024-12-02 07:17:29 -0300

    finishing

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps')
+const cssnano = require('cssnano');
 
 //IMAGENES
 const imagemin = require('gulp-imagemin');
@@ -17,8 +19,10 @@ function css( done ) {
   //pasos: 1 - identificar archivos, 2 -compilar, 3 - guardar el .css
 
   src('src/scss/app.scss')
+    .pipe( sourcemaps.init() )
     .pipe( sass() )
-    .pipe( postcss([ autoprefixer() ]) )
+    .pipe( postcss([ autoprefixer(), cssnano() ]) )
+    .pipe( sourcemaps.write('.') )
     .pipe( dest('build/css') )
   
   done();
